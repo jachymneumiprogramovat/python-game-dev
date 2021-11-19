5b9a16def02b5d9a496adb294a22512ed55976ba
jachymneumiprogramovat <82580668+jachymneumiprogramovat@users.noreply.github.com>
2021-11-19 15:30:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import turtle as tt
 
-GRID_SIZE = 450
-FIELD_SIZE = GRID_SIZE // 3
-RADIUS = (3 * FIELD_SIZE) // 4
+HraciPole = 600
+Policko = HraciPole // 3
+poloměr = (3 * Policko) // 8
+hranice = HraciPole // 2
 
 pen = tt.Turtle()
 screen = tt.Screen()
 
 
-def draw_square(side_length):
+def ctverec(side_length):
     # Draw a square of side length 'side'.
     for _ in range(4):
         pen.forward(side_length)
         pen.left(90)
 
 
-def draw_circle(x, y, radius):
+def kolecko(x, y, radius):
     # Draw a circle of radius 'radius' at (x, y).
     pen.tiltangle(0)
     pen.penup()
@@ -24,7 +25,7 @@ def draw_circle(x, y, radius):
     pen.circle(radius)
 
 
-def draw_cross(x, y, radius):
+def krizek(x, y, radius):
     # Draw a cross centered at (x, y) of radius 'radius'.
     pen.penup()
     pen.goto(x - radius, y - radius)
@@ -36,7 +37,7 @@ def draw_cross(x, y, radius):
     pen.goto(x + radius, y - radius)
 
 
-def draw_grid(x, y, side_length):
+def tabulka(x, y, side_length):
     third = side_length // 3
     pen.pu()
     pen.goto(x, y)
@@ -56,15 +57,54 @@ def draw_grid(x, y, side_length):
         pen.goto(x, y + (i + 1) * third)
 
 
-def handle_click(x, y):
-    pass
+def kliknuti(x, y):
+    global player, pole
+    if abs(x) <= hranice and abs(y) <= hranice:
+        row = (-int(y) + hranice) // Policko
+        col = (int(x) + hranice) // Policko
+
+        if pole[row][col]==0:
+            pole[row][col] = player
+        else:
+            print("tí")
+            return
+
+        center = (
+            -hranice + col * Policko + Policko // 2,
+            hranice - row * Policko - Policko // 2
+        )
+
+        if player == 1:
+            krizek(center[0], center[1], poloměr)
+        else:
+            kolecko(center[0], center[1], poloměr)
+        kdovyhral()
+        player = -player
 
 
-board = [
+def kdovyhral():
+    for i in pole:
+        if abs(sum(i))==3:
+            print("vyhral")
+    for i in range(3):
+        if abs(pole[0][i]+pole[1][i]+pole[2][i])==3 :
+            print("vyhral")
+    
+    if abs(pole[0][0]+pole[1][1]+pole[2][2])==3:
+        print("vyhral")
+    
+    if abs(pole[0][2]+pole[1][1]+pole[2][0])==3:
+        print("vyhral")
+pole = [
     [0, 0, 0],
     [0, 0, 0],
     [0, 0, 0]
 ]
-screen.onclick(handle_click)
+player = 1
+screen.onclick(kliknuti)
+pen.speed(100)
+pen.hideturtle()
+
+tabulka(-hranice, -hranice, HraciPole)
 
 tt.mainloop()
